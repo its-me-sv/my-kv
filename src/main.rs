68da961de81d700d6e70a8dd3ace13d765471add
Suraj Vijay <surajcpp115@gmail.com>
2023-06-30 12:40:18 +0530

    accepting client connections and echoing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+const ADDRESS: &str = "127.0.0.1:8080";
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind(ADDRESS)
+        .unwrap_or_else(|_| panic!("[SERVER] Couldn't bind to {ADDRESS}"));
+    println!("[SERVER] Binded to {ADDRESS}");
+
+    for client_conn in server.incoming() {
+        match client_conn {
+            Ok(client) => {
+                std::thread::spawn(move || {
+                    println!(
+                        "[SERVER] Client {:#?} connected",
+                        client.peer_addr().unwrap()
+                    );
+                    handle_client(client);
+                });
+            }
+            Err(e) => eprintln!("[SERVER] Error: {e} | Couldn't accept conncetion from client"),
+        }
+    }
+}
+
+fn handle_client(mut client: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    loop {
+        match client.read(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    println!(
+                        "[SERVER] Client {:#?} disconnected",
+                        client.peer_addr().unwrap()
+                    );
+                    break;
+                }
+                let request = String::from_utf8_lossy(buffer.as_slice()).to_string();
+                println!(
+                    "[SERVER] Client {:#?} sent {}",
+                    client.peer_addr().unwrap(),
+                    request
+                );
+                client.write_all(&buffer[..size]).unwrap();
+            }
+            Err(e) => {
+                eprintln!("[SERVER] Error: {e} | Read error occurred");
+                break;
+            }
+        }
+    }
 }
